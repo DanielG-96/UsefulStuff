f0db1d218bba8d8eb798abdb404342eca99ececb
DanielG-96 <danielg13@live.com.au>
2017-01-17 11:41:40 +1000

    Deleted obsolete ToReadableTime method since it isn't really reliable.

--- UsefulStuffPortable/Extensions.cs
+++ UsefulStuffPortable/Extensions.cs
@@ -169,53 +169,6 @@ namespace UsefulStuffPortable.Extensions
         }
 
         /// <summary>
-        /// Gets a readable sentence from a time (e.g. 2 seconds ago, 1 month ago)
-        /// </summary>
-        /// <param name="value">Date/time to get value from</param>
-        /// <returns>Readable time</returns>
-        [Obsolete("This method doesn't really work properly, use ToPrettyDate instead.")]
-        public static string ToReadableTime(this DateTime value)
-        {
-            var ts = new TimeSpan(DateTime.UtcNow.Ticks - value.Ticks);
-            double delta = ts.TotalSeconds;
-            if (delta < 60)
-            {
-                return ts.Seconds == 1 ? "one second ago" : ts.Seconds + " seconds ago";
-            }
-            if (delta < 120)
-            {
-                return "a minute ago";
-            }
-            if (delta < 2700) // 45 * 60
-            {
-                return ts.Minutes + " minutes ago";
-            }
-            if (delta < 5400) // 90 * 60
-            {
-                return "an hour ago";
-            }
-            if (delta < 86400) // 24 * 60 * 60
-            {
-                return ts.Hours + " hours ago";
-            }
-            if (delta < 172800) // 48 * 60 * 60
-            {
-                return "yesterday";
-            }
-            if (delta < 2592000) // 30 * 24 * 60 * 60
-            {
-                return ts.Days + " days ago";
-            }
-            if (delta < 31104000)
-            {
-                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
-                return months <= 1 ? "one month ago" : months + " months ago";
-            }
-            var years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
-            return years <= 1 ? "one year ago" : years + " years ago";
-        }
-
-        /// <summary>
         /// Gets a readable date that looks pretty (e.g. 2 minutes ago, 6 hours ago etc.)
         /// </summary>
         /// <param name="date">Date to check</param>
