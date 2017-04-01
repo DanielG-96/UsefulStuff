9f65d7ab61ecc4d9279ac457104ac94647b13a79
DanielG-96 <danielg13@live.com.au>
2017-04-02 08:13:22 +1000

    Changed test program

--- USPTests/Program.cs
+++ USPTests/Program.cs
@@ -12,14 +12,21 @@ namespace USPTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Current time is: " + DateTime.UtcNow);
-            DateTime earlierTime = DateTime.UtcNow.Subtract(new TimeSpan(0, 0, 1, 30));
+            Console.WriteLine("Current time is: " + DateTime.Now);
+            DateTime earlierTime = DateTime.Now.Subtract(new TimeSpan(0, 0, 1, 30));
             Console.WriteLine(earlierTime);
             Console.WriteLine(earlierTime.ToPrettyDate());
 
             Console.WriteLine(Utilities.SizeSuffix(100005000L));
 
             Console.WriteLine(Utilities.GetRandomLetter(true));
+
+            bool isWeekend = DateTime.Now.IsWeekend();
+
+            if (isWeekend)
+                Console.WriteLine("Today is a weekend");
+            else
+                Console.WriteLine("Today is NOT a weekend");
 
             Console.ReadLine();
         }
